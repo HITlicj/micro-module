0fccf2e3d374a0dc9a03125e701dc3f5dc88b561
HITlicj <17611269916@163.com>
2023-04-17 19:51:22 +0800

    feat: sandbox

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: [require.resolve('@atom-web/fabric/lib/eslint')],
   rules: {
     'react/require-default-props': 0,
-    'no-console': "warn",
+    'no-console': 'warn',
     'prefer-rest-params': 0,
     'no-new-func': 0,
   },
